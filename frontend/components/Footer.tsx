979093a76793478c93c16a08d175adb0d0aef950
Daian Scuarissi <scuarissid@gmail.com>
2022-11-17 03:34:28 +0100

    Revert fix

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -2,8 +2,6 @@ const Footer = () => {
   return (
     <footer className="h-32 mt-auto bg-[#042541] flex justify-center items-center">
       <span className="text-white text-lg font-bold">Develop by D.S</span>
-
-      
     </footer>
   );
 };
